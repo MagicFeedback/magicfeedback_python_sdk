8773e60d0fc8695cd96ef7bad092a77195afae49
resarnau <resarnau@gmail.com>
2026-04-23 15:47:37 +0200

    v1.0.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="magicfeedback_sdk",
-    version="1.0.9",
+    version="1.0.10",
     description="A Python SDK for interacting with the MagicFeedback API",
     author="Francisco Arias",
     author_email="farias@magicfeedback.io",
